4f35ff5883c3dba2cfd378d6913d641193cc3549
asmaTriki123 <97709433+asmaTriki123@users.noreply.github.com>
2025-01-02 00:44:47 +0100

    Update ChambreService.java

--- src/main/java/tpasma/service/ChambreService.java
+++ src/main/java/tpasma/service/ChambreService.java
@@ -2,9 +2,6 @@ package tpasma.service;
 
 import java.util.List;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
 import org.springframework.stereotype.Service;
