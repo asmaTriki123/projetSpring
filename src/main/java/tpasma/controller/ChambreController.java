30c58bb8df6c4d0f0d1b7efb0028be48438d670c
asmaTriki123 <97709433+asmaTriki123@users.noreply.github.com>
2025-01-02 00:49:39 +0100

    Update ChambreController.java

--- src/main/java/tpasma/controller/ChambreController.java
+++ src/main/java/tpasma/controller/ChambreController.java
@@ -2,8 +2,6 @@ package tpasma.controller;
 
 import java.util.List;
 import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
